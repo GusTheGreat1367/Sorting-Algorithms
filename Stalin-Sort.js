4c01c5c62ea689daaa7333cd5b6456b1c0410dce
Gus <guspearson78@gmail.com>
2026-03-25 19:38:52 -0700

    Refactor StalinSort for improved clarity and performance
    Refactor StalinSort function to simplify sorting logic.

--- Stalin-Sort.js
+++ Stalin-Sort.js
@@ -1,17 +1,11 @@
-// Using an Array named arr
 async function StalinSort(arr) {
-  function isSorted(arr) {
-    for (let i = 0; i < arr.length - 1; i++) {
-      if (arr[i] > arr[i + 1]) return false;
-      else return true;
+  let i = 0;
+  while(i < arr.length - 1) {
+    if (arr[i] > arr[i + 1]) {
+        arr.splice(i, 1);
     }
-  }
-
-  while (!isSorted(arr)) {
-    for (let j = 0; j < arr.length - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        arr.splice(arr[j], 1);
-      }
+    else{ 
+      i++;
     }
   }
   return arr;
