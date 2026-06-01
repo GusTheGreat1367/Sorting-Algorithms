e33d1ad45291448175c116db5578dd243e57dc54
Gus <guspearson78@gmail.com>
2026-06-01 09:02:44 -0700

    Update Stalin-Sort.js

--- Stalin-Sort.js
+++ Stalin-Sort.js
@@ -2,7 +2,7 @@
 function StalinSort(arr) {
   let i = 0;
   while(i < arr.length - 1) {
-    while (arr[i] > arr[i + 1]) {
+    if (arr[i] > arr[i + 1]) {
         arr.splice(i+1, 1);
     }
     i++;
